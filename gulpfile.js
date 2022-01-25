43d7c006815f2543debf52e9bdc42df6fb78c790
SeongHoon Wee <mrgravity817@gmail.com>
2022-01-25 17:32:52 +0900

    Purge unused css

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,16 @@
 const {src, dest, watch, series} = require('gulp');
 const sass = require('gulp-sass')(require('sass'))
+const purgecss = require('gulp-purgecss');
 
 const buildStyles = () => {
 	return src('orange/**/*.scss')
 		.pipe(sass())
+		.pipe((purgecss({content: ['*.html']})))
 		.pipe(dest('css'))
 }
 
 const watchTask = ()=> {
-	watch(['orange/**/*.scss'], buildStyles)
+	watch(['orange/**/*.scss', '*.html'], buildStyles)
 }
 
 exports.default = series(buildStyles, watchTask);
